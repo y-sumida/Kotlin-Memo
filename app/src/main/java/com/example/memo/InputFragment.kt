82c96f9749fcf028527900be5a05911b584581eb
Yuki Sumida <y.sumida.0201@gmail.com>
2019-05-10 23:16:47 +0900

    メモ入力用フラグメントを実装

---
+++ app/src/main/java/com/example/memo/InputFragment.kt
@@ -0,0 +1,51 @@
+package com.example.memo
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_input.*
+import java.io.File
+
+class InputFragment : Fragment() {
+    interface OnFileOutputListener {
+        fun onFileOutput()
+    }
+
+    private var currentFile : File? = null
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        currentFile?.let {
+            outState.putSerializable("file", it)
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (savedInstanceState != null && savedInstanceState.containsKey("file")) {
+            currentFile = savedInstanceState.getSerializable("file") as File
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_input, container, false)
+
+        save.setOnClickListener {
+            currentFile = outputFile(currentFile, content.text.toString())
+            if (context is OnFileOutputListener) {
+                (context as OnFileOutputListener).onFileOutput()
+            }
+        }
+
+        return view
+    }
+
+    fun show(file: File) {
+        val memo = inputFile(file)
+        content.setText(memo)
+
+        currentFile = file
+    }
+}
