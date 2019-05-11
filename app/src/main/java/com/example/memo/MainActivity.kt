0da1085c6fbfe9a2d5fcee917af301dd9b3a5b77
Yuki Sumida <y.sumida.0201@gmail.com>
2019-05-12 00:13:00 +0900

    アクティビティの実装

--- app/src/main/java/com/example/memo/MainActivity.kt
+++ app/src/main/java/com/example/memo/MainActivity.kt
@@ -1,12 +1,91 @@
 package com.example.memo
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.content.res.Configuration
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),
+    FileListFragment.OnFilesSelectListener,
+    InputFragment.OnFileOutputListener {
+
+    private var drawerToggle : ActionBarDrawerToggle? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (hasPermission()) setViews()
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        drawerToggle?.syncState()
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration?) {
+        super.onConfigurationChanged(newConfig)
+        drawerToggle?.onConfigurationChanged(newConfig)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (drawerToggle?.onOptionsItemSelected(item) == true) {
+            return true
+        } else {
+            return super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onFileSelected(file: File) {
+        val fragment = supportFragmentManager.findFragmentById(R.id.input) as InputFragment
+        fragment.show(file)
+    }
+
+    override fun onFileOutput() {
+        val fragment = supportFragmentManager.findFragmentById(R.id.list) as FileListFragment
+        fragment.show()
+    }
+
+    private fun setupDrawer(drawer: DrawerLayout) {
+        val toggle = ActionBarDrawerToggle(this, drawer, R.string.app_name, R.string.app_name)
+        toggle.isDrawerIndicatorEnabled = true
+        drawer.addDrawerListener(toggle)
+
+        drawerToggle = toggle
+
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setHomeButtonEnabled(true)
+        }
+    }
+
+    private fun setViews() {
         setContentView(R.layout.activity_main)
+
+        if (drawerLayout != null) {
+            setupDrawer(drawerLayout)
+        }
+    }
+
+    private fun hasPermission() : Boolean {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), 1)
+            return false
+        }
+        return true
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (!grantResults.isEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            setViews()
+        } else {
+            finish()
+        }
     }
 }
